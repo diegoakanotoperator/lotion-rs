f6e4e79e3c883b8ef74b925765438065352ac724
Diego Aka Not Operator <diegoakanotoperator@users.noreply.github.com>
2026-03-06 17:38:30 -0300

    fix: resolve compilation error in menu.rs and add dmg target for macOS

--- src-tauri/src/menu.rs
+++ src-tauri/src/menu.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 use tauri::{
-    menu::{MenuBuilder, MenuItem, SubmenuBuilder},
+    menu::{AboutMetadata, MenuBuilder, MenuItem, SubmenuBuilder},
     AppHandle, Manager,
 };
 
